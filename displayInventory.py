a6f395d5fabc5690835bee8059d49d25e22e3c84
Alex Staelens <Alex.Staelens@vu.com>
2023-03-24 14:39:18 -0500

    Added for loop to print out each entry of the inventory and a counting variable to calc the total

--- displayInventory.py
+++ displayInventory.py
@@ -2,7 +2,11 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
     # your code goes here
-
-
+    print('Inventory:')
+    count=0
+    for i in inventory:
+        print(inventory[i], i)
+        count+=inventory[i]
+    print('Total number of items:', count)
 if __name__ == "__main__":
     displayInventory(stuff)
